be91f33ba8514f5f433b64f3a90235860124c079
James Conroy <jconroy12@gmail.com>
2019-09-08 01:26:08 -0500

    Added Comments

--- clockInOut.sh
+++ clockInOut.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
+# A script to keep track of clocking in and out of work
 
 TIME=$(date +"%a, %b %_d -%_I:%M %p")
 
+# Get the second word at the head of file
 STATUS=$(head -n1 $1 | awk '{print $2}')
 
+# Set the status based on it's current state.
 if [ "$STATUS" != "in" ];
 then STATUS="in "
 else STATUS="out"
 fi
 
+# Insert output to the head of the file
 echo "Clock $STATUS $TIME
 $(cat $1)" > $1
