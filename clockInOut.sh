ee0117aeef112814bbddc4dbd56c394bd8f13714
James Conroy <jconroy12@gmail.com>
2019-09-13 08:37:29 -0500

    Check Input Esists
    clockInOut now checks to see if the input files already exists
    This is to prevent the error messages when you tried to head and awk a
    nonexistent file

--- clockInOut.sh
+++ clockInOut.sh
@@ -3,13 +3,19 @@
 
 TIME=$(date +"%a, %b %_d -%_I:%M %p")
 
-# Get the second word at the head of file
-STATUS=$(head -n1 $1 | awk '{print $2}')
+# Check to see if $1 exists
+if [ -f $1 ]
+then
+	# Get the second word at the head of file
+	STATUS=$(head -n1 $1 | awk '{print $2}')
 
-# Set the status based on it's current state.
-if [ "$STATUS" != "in" ];
-then STATUS="in "
-else STATUS="out"
+	# Set the status based on it's current state.
+	if [ "$STATUS" != "in" ];
+	then STATUS="in "
+	else STATUS="out"
+	fi
+else
+	STSTUS="out"
 fi
 
 # Insert output to the head of the file
